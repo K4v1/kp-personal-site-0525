c28e63391695f98d7b477c97726823d3e786c5c9
Kavi <60553508+K4v1@users.noreply.github.com>
2025-05-26 00:06:34 +0200

    Add global footer and update styles

--- pages/index.js
+++ pages/index.js
@@ -72,19 +72,6 @@ export default function Home({ posts }) {
               <a href="#" className="hover:text-accent">More…</a>
             </div>
           </section>
-          <footer className="mt-16 border-t pt-8">
-            <div className="flex flex-wrap gap-6 md:gap-8 text-sm">
-              <div className="space-y-2">
-                <p><strong>Kavi Pather</strong></p>
-                <p><a href="mailto:email@example.com" className="text-blue-600 hover:text-accent">email@example.com</a></p>
-              </div>
-              <div className="space-y-2">
-                <p><strong>EY</strong></p>
-                <p>Johannesburg</p>
-              </div>
-              <div className="ml-auto">© Kavi Pather</div>
-            </div>
-          </footer>
         </main>
       </Container>
     </>
